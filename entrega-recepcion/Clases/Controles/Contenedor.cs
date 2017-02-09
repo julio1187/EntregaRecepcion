e06d9c8cd70215716855f432464e32393ef92349
JULIO BELTRAN MEZA <asesor@guasave.gob.mx>
2017-02-09 14:24:28 -0700

    estructura

---
+++ entrega-recepcion/Clases/Controles/Contenedor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace entrega_recepcion.Clases.Controles
+{
+    public class Contenedor : PlaceHolder
+    {
+    }
+}
